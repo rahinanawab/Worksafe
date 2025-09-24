fb3b963dfaeab1b70622588b2bf648431b36ceb0
rahinanawab <rahinamerchant@gmail.com>
2025-09-24 05:25:16 -0700

    commit

--- src/test/java/pageObjects/RemoveTeam.java
+++ src/test/java/pageObjects/RemoveTeam.java
@@ -1,45 +1,48 @@
 package pageObjects;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
-
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-public class RemoveTeam extends BasePage{
+public class RemoveTeam extends BasePage {
     private final By removeteam = By.xpath("(//tr[contains(@class,'ant-table-row')][1]/td[3]//p)[1]");
     private final By selectCheckboxes = By.cssSelector("input.ant-checkbox-input");
     private final By removeButton = By.xpath("//button[normalize-space()='Remove user']");
 
-    public void removemember() throws InterruptedException {
-
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
-        Thread.sleep(1000);
-        WebElement member = driver.findElement(removeteam);
+    public void removemember() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        WebElement member = wait.until(ExpectedConditions.elementToBeClickable(removeteam));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", member);
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();", member);
 
-        List<WebElement> checkboxes = driver.findElements(selectCheckboxes);
-        if (checkboxes.size() < 2) {
-            System.out.println("Less than 2 checkboxes found!");
-            return;
-        }
+        List<WebElement> checkboxes = wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(selectCheckboxes, 1));
+
         Collections.shuffle(checkboxes, new Random());
-        for (int i = 0; i < 2; i++) {
-            WebElement checkboxInput = checkboxes.get(i);
-            if (!checkboxInput.isSelected()) {
+
+        int selected = 0;
+        for (WebElement checkbox : checkboxes) {
+            if (selected >= 2) break;
+            try {
+                if (!checkbox.isSelected()) {
+                    ((JavascriptExecutor) driver).executeScript("arguments[0].click();", checkbox);
+                    selected++;
+                }
+            } catch (ElementClickInterceptedException | StaleElementReferenceException e) {
+                System.out.println("Retrying click due to: " + e.getMessage());
                 try {
-                    WebElement label = checkboxInput.findElement(By.xpath("./ancestor::label"));
-                    label.click();
-                } catch (Exception e) {
-                    System.out.println("Could not click checkbox " + i + ": " + e.getMessage());
+                    ((JavascriptExecutor) driver).executeScript("arguments[0].click();", checkbox);
+                    selected++;
+                } catch (Exception ex) {
+                    System.out.println("Failed to click checkbox after retry.");
                 }
             }
         }
 
-        driver.findElement(removeButton).click();
+        WebElement removeBtn = wait.until(ExpectedConditions.elementToBeClickable(removeButton));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", removeBtn);
     }
 }
--- src/test/java/pageObjects/SearchUser.java
+++ src/test/java/pageObjects/SearchUser.java
@@ -1,25 +1,29 @@
 package pageObjects;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class SearchUser extends BasePage {
 
-//    private final By userstab = By.xpath("//*[@aria-describedby='«r7»']");
-private final By userstab = By.cssSelector("div[class='_sideBarScroller_123ft_14'] div:nth-child(2)");
-
+    private final By userstab = By.cssSelector("div[class='_sideBarScroller_123ft_14'] div:nth-child(2)");
     private final By searchemailField = By.xpath("//input[@placeholder='Search user']");
+    private final By overlayMask = By.cssSelector(".ant-drawer-mask");
 
-    public void  users() throws InterruptedException {
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
-        Thread.sleep(1000);
-        driver.findElement(userstab).click();
+    public void users() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(overlayMask));
+        WebElement users = wait.until(ExpectedConditions.elementToBeClickable(userstab));
+        users.click();
     }
-    public void enterEmailsearch(String email) throws InterruptedException {
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
-        driver.findElement(searchemailField).sendKeys(email);
-        Thread.sleep(3000);
 
+    public void enterEmailsearch(String email) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(searchemailField));
+        emailField.clear();
+        emailField.sendKeys(email);
     }
 }
