0a8627f348f7d379544493887b7acf55bcc53bbb
rahinanawab <rahinamerchant@gmail.com>
2025-09-24 01:53:58 -0700

    commit

---
+++ src/test/java/TestCases/RemoveTeamTest.java
@@ -0,0 +1,12 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+import pageObjects.RemoveTeam;
+
+public class RemoveTeamTest extends BaseTest {
+    @Test(priority = 5, dependsOnMethods = "TestCases.LoginTest.testLogin")
+    public void removeteam() throws InterruptedException {
+        removeTeam = new RemoveTeam();
+        removeTeam.removemember();
+    }
+}
